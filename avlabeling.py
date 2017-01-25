cb32dac8ff8612f353c95548a79c4f670764404e
dso <deeso@users.noreply.github.com>
2017-01-25 17:14:36 -0600

    Update avlabeling.py

--- avlabeling.py
+++ avlabeling.py
@@ -1,4 +1,8 @@
 import subprocess, traceback, os, sys, sqlite3
+from datetime import datetime
+
+def time_str():
+    return str(datetime.now().strftime("%H:%M:%S.%f %m-%d-%Y"))
 
 CLAMSCAN_ARGS = "clamscan {target_files}"
 def create_clamscan_cmd(target_files):
@@ -11,6 +15,7 @@ def perform_clamscan(target_files):
     #print ("Experiment Log: zip command: %s and list:[%s]"%(zip_command, zip_command.split()))
     data = None
     try:
+        #print clamscan_command.split()
         p = subprocess.Popen(clamscan_command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         #CP_LOCK.acquire()
         #COMPRESSION_PROCS.append((p, dump_file, True))
@@ -29,12 +34,20 @@ def perform_clamscan(target_files):
 SPLIT_ON_THIS = '''----------- SCAN SUMMARY -----------'''
 LABEL_SEP = ': '
 VIRUS_SHARE_HASH = "VirusShare_"
+TAIL = ' FOUND'
 def post_process_data(raw_results, base_location=None):
     results = raw_results.split(SPLIT_ON_THIS)[0].strip()
     lines = [i for i in results.splitlines()]
     hash_results = {}
     for line in lines:
+        if line.find(LABEL_SEP) == -1:
+            print ("[X] Line does not contain a LABEL_SEP separator: %s"%line)
+            continue
         f, l = line.split(LABEL_SEP)
+        l = l.strip(TAIL)
+        if f.find(VIRUS_SHARE_HASH) == -1:
+            print ("[X] File entry does not contain a VIRUS_SHARE separator: %s"%f)
+            continue
         h = f.split(VIRUS_SHARE_HASH)[1].strip()
         hash_results[h] = l.strip()
     return hash_results
@@ -60,27 +73,35 @@ def init_database(output_sqllite_db):
 
 
 if __name__ == '__main__':
+    #print sys.argv
     base_location = sys.argv[1]
     start = int(sys.argv[2])
     end = int(sys.argv[3])
     output_sqllite_db = sys.argv[4]
-
+    stime = datetime.now()
     samples = read_samples_directory(base_location)
+    end = len(samples) if end > len(samples) else end
     the_files = samples[start:end]
     hashes_labels = {}
     pos = 0
     window = 200
-    while pos < end:
+    _end = len(the_files)
+    while pos < _end:
+        window = window if pos+window < _end else _end-pos+1
         target_files = the_files[pos:pos+window]
         res = perform_clamscan(target_files)
         _hashes_labels = post_process_data(res)
         hashes_labels.update(_hashes_labels)
-        print ("processed %d files @ %d"%(len(_hashes_labels), pos))
+        print ("[=] Processed %d files @ %d"%(len(_hashes_labels), pos+start))
         pos += window
 
     if len(hashes_labels) > 0:
         write_files_results(hashes_labels, output_sqllite_db)
 
+    etime = datetime.now()
+
+    print ("Started: %s"%stime.strftime("%H:%M:%S.%f %m-%d-%Y"))
+    print ("Ended: %s"%etime.strftime("%H:%M:%S.%f %m-%d-%Y"))
 # python avlabeling.py /research_data/test/   0 2000 /research_data/hash_labels.db
 
 #python avlabeling.py /research_data/test/   0 5700 /research_data/hash_labels.db &
