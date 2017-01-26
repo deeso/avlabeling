1b97f473245cb919461a83060024b0d62fd56bda
dso <deeso@users.noreply.github.com>
2017-01-25 18:33:40 -0600

    Committing progress on fpscan remote work

--- fpscan_avlabeling_remote.py
+++ fpscan_avlabeling_remote.py
@@ -1,35 +1,74 @@
-import subprocess, traceback, os, sys, sqlite3, json
+import sys, argparse, threading, json
+import subprocess, traceback, os, sys, sqlite3
+from basic_multi_host_commands import *
+
 from datetime import datetime
 
 def time_str():
     return str(datetime.now().strftime("%H:%M:%S.%f %m-%d-%Y"))
 
-FPSCAN_ARGS = "fpscan --report --applications --adware {target_files}"
-def create_fpscan_cmd(target_files):
-    keyed = {'target_files':" ".join(target_files)}
-    return FPSCAN_ARGS.format(**keyed)
 
-def perform_fpscan(target_files):
-    fpscan_command = create_fpscan_cmd(target_files)
-    #print ("Experiment Log: zipiing the memory file %s"%dump_file)
-    #print ("Experiment Log: zip command: %s and list:[%s]"%(zip_command, zip_command.split()))
-    data = None
-    try:
-        #print clamscan_command.split()
-        p = subprocess.Popen(clamscan_command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        #CP_LOCK.acquire()
-        #COMPRESSION_PROCS.append((p, dump_file, True))
-        try:
-            data = p.communicate()[0]
-        except:
-            traceback.print_exc()
-        res = 0
-    except:
-        traceback.print_exc()
-    finally:
-        # CP_LOCK.release()
-        pass
-    return data
+parser = argparse.ArgumentParser(description="Perform fpscan on remote Linux hosts with paramiko")
+parser.add_argument('-user', help='user to log in with', type=str)
+parser.add_argument('-password', help='user to log in with', type=str)
+parser.add_argument('-hosts', help='hosts to connect to', nargs='*' type=str)
+parser.add_argument('-base_location', help='local directory base location (I mounted with NFS)', nargs='*' type=str)
+parser.add_argument('-avlabel_location', help='local directory base location (I mounted with NFS)', nargs='*' type=str)
+parser.add_argument('-total_files', help='Number of files so work is spread evenly', type=int)
+parser.add_argument('-sqlite_location', help='Location of the sqlite_db', type=int)
+
+START_DATA_TERM = "=====START_DATA_DUMP====="
+END_DATA_TERM = "=====END_DATA_DUMP====="
+# print ("Started: %s"%stime.strftime("%H:%M:%S.%f %m-%d-%Y"))
+# print ("Ended: %s"%etime.strftime("%H:%M:%S.%f %m-%d-%Y"))
+COMPLETED_TERM = "=====COMPLETED====="
+FPSCAN_AVLABEL_CMD = "python {avlabel_dir}/fpscan_avlabeling_remote.py {base_location} {start} {end}"
+def generate_command(avlabel_location, base_location, start, end):
+    cmd_dict = {'avlabel_location':avlabel_location, 'base_location':base_location, 'start':start, 'end':end}
+    cmd = FPSCAN_AVLABEL_CMD.format(**cmd_dict)
+    return cmd
+
+def execute_remote_command(username, password, host, command, output_sqllite_db):
+    data = exec_cmds(host, username, password, [command, ])
+    process = True
+    if data.find(START_DATA_TERM) == -1:
+        print ("[X] Failed to run command and get back data, no data start")
+        process = False
+
+    if data.find(END_DATA_TERM) == -1:
+        print ("[X] Failed to run command and get back data, no data end")
+        process = False
+
+    raw_results = data.split(START_DATA_TERM)[1].split(END_DATA_TERM)[0]
+    # XXX This is pretty dangerous, evaluating the Python dictionary to get an object
+    # json might be better
+    hash_results = json.loads(raw_results)
+    if len(hashes_labels) > 0:
+        write_files_results(hashes_labels, output_sqllite_db, 'fpscan')
+
+    if data.find(COMPLETED_TERM) > 0:
+        completed = data.split(END_DATA_TERM)[1].split(COMPLETED_TERM)[0]
+        print (completed)
+
+
+def write_files_results(hash_results, output_sqllite_db, av_engine='clamav'):
+    conn = sqlite3.connect(output_sqllite_db)
+    c = conn.cursor()
+    rows = []
+    for h,l_m in hash_results.items():
+        l, m = l_m
+        rows.append((h,l, m, av_engine))
+    c.executemany("INSERT INTO hash_labels VALUES (?,?,?,?)", rows)
+    conn.commit()
+
+
+def init_database(output_sqllite_db):
+    conn = sqlite3.connect(output_sqllite_db)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE hash_labels (hash, label, meta, av_engine)''')
+    c.close()
+    conn.commit()
+
 
 LABEL_SEP = '] <'
 VIRUS_SHARE_HASH = "VirusShare_"
@@ -51,59 +90,70 @@ def post_process_data(raw_results, base_location=None):
         hash_results[h] = (label, line.strip())
     return hash_results
 
-def read_samples_directory(base_location):
-    files = [os.path.join(base_location, i) for i in os.listdir(base_location)]
-    return files
-
-def write_files_results(hash_results, output_sqllite_db, av_engine='clamav'):
-    conn = sqlite3.connect(output_sqllite_db)
-    c = conn.cursor()
-    rows = []
-    for h,l_m in hash_results.items():
-        l, m = l_m
-        rows.append((h,l, m, av_engine))
-    c.executemany("INSERT INTO hash_labels VALUES (?,?,?,?)", rows)
-    conn.commit()
 
 
-def init_database(output_sqllite_db):
-    conn = sqlite3.connect(output_sqllite_db)
-    c = conn.cursor()
-    c.execute('''CREATE TABLE hash_labels (hash, label, meta, av_engine)''')
-    c.close()
-    conn.commit()
+def perform_fp_scan_hosts(username, password, host_list, commands, output_sqllite_db):
+    threads = []
+    pos = 0
+    while pos < len(commands):
+        command = commands[pos]
+        host = host_list[pos%len(host_list)]
+        t = threading.Thread(target=execute_remote_command, args=(username, password, host, command, output_sqllite_db))
+        print ("[=] %s Started Host (%s) command: %s"%(time_str(), host, command))
+        t.start()
+        threads.append(t)
+
+    print ("[=] %s All commands started waiting for the threads to complete."%(time_str()))
+    for t in threads:
+        t.join()
 
 
 if __name__ == '__main__':
-    #print sys.argv
-    base_location = sys.argv[1]
-    start = int(sys.argv[2])
-    end = int(sys.argv[3])
-    #output_sqllite_db = sys.argv[4]
-    stime = datetime.now()
-    samples = read_samples_directory(base_location)
-    end = len(samples) if end > len(samples) else end
-    the_files = samples[start:end]
-    hashes_labels = {}
+    args = parser.parse_args()
+
+    if args.user is None or args.password is None:
+        print ("[X] Must provide user name and password")
+        sys.exit(1)
+
+    if args.hosts is None or len(args.hosts) == 0:
+        print ("[X] Must provide hosts")
+        sys.exit(1)
+
+    if args.base_location is None:
+        print ("[X] Must provide base location to look up")
+        sys.exit(1)
+
+    if args.total_files is None:
+        print ("[X] Must provide total files, please")
+        sys.exit(1)
+
+    if args.sqlite_location is None:
+        print ("[X] Must provide sqlite location")
+        sys.exit(1)
+
+    try:
+        os.stat(args.sqlite_location)
+    except:
+        init_database(args.sqlite_location)
+
+
+    window = long(float(total_files) / len(hosts))
+    extra = 0
+
+    if total_files != window * len(hosts):
+        extra = total_files - (window * len(hosts))
+
+    offsets = []
     pos = 0
-    window = 200
-    _end = len(the_files)
-    while pos < _end:
-        window = window if pos+window < _end else _end-pos+1
-        target_files = the_files[pos:pos+window]
-        res = perform_fpscan(target_files)
-        _hashes_labels = post_process_data(res)
-        hashes_labels.update(_hashes_labels)
-        print ("[=] Processed %d files @ %d"%(len(_hashes_labels), pos+start))
-        pos += window
+    offset = 0
+    commands = []
+    cnt = 0
+    while pos < len(hosts):
+        start = offset
+        end = offset+window
+        cmd = generate_command(avlabel_location, base_location, start, end)
+        if extra > 0 and pos == len(hosts) -1:
+            cmd = generate_command(avlabel_location, base_location, start, end+extra)
+        commands.append(cmd)
 
-    #if len(hashes_labels) > 0:
-    #    write_files_results(hashes_labels, output_sqllite_db, 'fpscan')
-
-    etime = datetime.now()
-    print ("=====START_DATA_DUMP=====")
-    print (json.dumps(hashes_labels))
-    print ("=====END_DATA_DUMP=====")
-    print ("Started: %s"%stime.strftime("%H:%M:%S.%f %m-%d-%Y"))
-    print ("Ended: %s"%etime.strftime("%H:%M:%S.%f %m-%d-%Y"))
-    print ("=====COMPLETED=====")
+    perform_fp_scan_hosts(args.user, args.password, args.hosts, commands, args.sqlite_location)
